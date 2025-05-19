01d02aa18dc9301e52ef0ea7fc8d761067da11c2
Du-TI-BMZ <bmzeduardosochodolak@gmail.com>
2025-05-19 15:25:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,11 +108,6 @@ TEMPO_ESPERA_CONFIRMACAO = 15  # segundos (pode ajustar)
 
 import ticketsup
 
-@bot.event
-async def on_ready():
-    await ticketsup.setup(bot)
-
-
 async def confirmar_estabilidade(arquivo):
     """Espera alguns segundos e confirma se o arquivo parou de ser modificado."""
     try:
@@ -368,7 +363,14 @@ async def cargo(ctx):
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot conectado como {bot.user}")
+    print(f"{bot.user.name} está online.")
+    
+    # Canal onde você quer que apareça o botão
+    canal = bot.get_channel(ID_DO_CANAL)
+    if canal:
+        view = TicketView()
+        await canal.send("📩 Clique abaixo para abrir um ticket de suporte:", view=view)
+
     try:
         bot.add_view(TicketButtonView())
         bot.add_view(SugestaoView())
@@ -381,6 +383,15 @@ async def on_ready():
         bot.loop.create_task(monitorar_pasta())      # Monitorar pasta
     except Exception as e:
         print(f"⚠️ Erro ao criar Tasks: {e}")
+
+
+async def main():
+    async with bot:
+        await bot.load_extension("ticketsup")
+        await bot.start("SEU_TOKEN")
+
+asyncio.run(main())
+
 
 def extrair_valor(texto, campo):
     try:
